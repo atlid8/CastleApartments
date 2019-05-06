1e5daaf634afef485d28df3ec222cb3fa418f6bc
Jóhannes Ingi Torfason <johannest18@ru.is>
2019-05-06 17:46:42 +0000

    database property

--- properties/models.py
+++ properties/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from user.models import user
 
 # Create your models here.
+class Property(models.Model):
+    name = models.CharField(max_length=255)
+    zip = models.IntegerField()
+    price = models.IntegerField()
+    commission = models.IntegerField()
+    rooms = models.IntegerField()
+    verified = models.BooleanField()
+    sellerID = models.ForeignKey(user, on_delete=models.CASCADE)
+    #Todo offers, address, viewcount
+    def __str__(self):
+        return self.name
+
+class propertyImage(models.Model):
+    image = models.CharField(max_length=999)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
