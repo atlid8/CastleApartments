b5833e3cd7dfdb0625e49a2fe201819059bb8be0
Aunnsteinsson <atliunnsteinsson@gmail.com>
2019-05-06 13:18:28 +0000

    Library for CA

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Atliu\Desktop\HR_2\Verklegt_namskeid2\CastleApartmentsDjango\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
