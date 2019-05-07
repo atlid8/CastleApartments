3afdaa8115da3c0f7fdaaaca6ba6450e1ae2e52f
Jóhannes Ingi Torfason <johannest18@ru.is>
2019-05-07 19:42:38 +0000

    hér er single property addað

--- properties/urls.py
+++ properties/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name="base-index"),
     path('properties/', views.properties, name="properties-index"),
+    path('properties/<int:id>', views.get_property_by_id, name="property_details"),
 ]
--- properties/views.py
+++ properties/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from properties.models import *
 
 # Create your views here.
@@ -9,6 +9,9 @@ def properties(request):
     context = {'castles': Property.objects.all()}
     return render(request, 'properties/properties-index.html', context)
 
-
+def get_property_by_id(request, id):
+    return render(request, 'properties/property_details.html',
+                   {'castle' : get_object_or_404(Property, pk=id)
+                    })
 
 
