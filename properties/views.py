d1f7f3f0c3032e4251215099ac51eb15791f705e
bbenediktssen <bennibje@gmail.com>
2019-05-07 16:50:32 +0000

    bla

--- properties/views.py
+++ properties/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from properties.models import *
 
 # Create your views here.
 def index(request):
     return render(request, 'base.html')
 
 def properties(request):
-    return render(request, 'properties/properties-index.html')
+    # context = {'castles': Property.objects.all()}
+    context2 = {'images': PropertyImage.objects.all()}
+    return render(request, 'properties/properties-index.html', context2)
+
+
+
 
 
