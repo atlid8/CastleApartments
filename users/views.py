0b1f287ee9530a09b2fd00e483224f55853ef2f6
steingrimure <steingrimure@gmail.com>
2019-05-07 15:16:28 +0000

    smá breytingar

--- users/views.py
+++ users/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def login(request):
     return render(request, 'users/login.html')
+
+def about_us(request):
+    return render(request, 'about_us.html')
