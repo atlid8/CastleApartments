b32465cb70dc4d5b412b7e2423e527ffe3a6dda0
Aunnsteinsson <atliunnsteinsson@gmail.com>
2019-05-07 21:01:21 +0000

    payments

--- properties/urls.py
+++ properties/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name="base-index"),
     path('properties/', views.properties, name="properties-index"),
     path('properties/<int:id>', views.get_property_by_id, name="property_details"),
+    path('payments/', views.payments, name="payments"),
 ]
