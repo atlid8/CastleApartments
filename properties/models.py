dd9ba91f32f515852f9bdb2076cdadbaa0b63554
Jóhannes Ingi Torfason <johannest18@ru.is>
2019-05-09 15:11:13 +0000

    database management

--- properties/models.py
+++ properties/models.py
@@ -10,7 +10,6 @@ class Property(models.Model):
     rooms = models.IntegerField()
     verified = models.BooleanField()
     info = models.TextField()
-    userinfo_id = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
     #Todo offers, address, viewcount
     def __str__(self):
         return self.name
@@ -20,18 +19,4 @@ class PropertyImage(models.Model):
     Property = models.ForeignKey(Property, on_delete=models.CASCADE)
     def __str__(self):
         return self.image
-    
-class NotVerified(models.Model):
-    name = models.CharField(max_length=255)
-    zip = models.IntegerField()
-    price = models.IntegerField()
-    commission = models.IntegerField()
-    rooms = models.IntegerField()
-    verified = models.BooleanField()
-    info = models.TextField()
-    userinfo_id = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
-    
-    class Meta:
-        managed = False
-        db_table = 'not_verified'
     
