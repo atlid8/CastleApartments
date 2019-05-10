e4abfaba5b93adc4d671129dd1630b6eb822a68a
Jóhannes Ingi Torfason <johannest18@ru.is>
2019-05-10 14:10:45 +0000

    sorry benni

--- properties/forms/create.py
+++ properties/forms/create.py
@@ -2,7 +2,7 @@ from django import forms
 from properties.models import Castle
 from properties.models import CastleImage
 
-class CastleCreationFrom(forms.ModelForm):
+class CastleCreationForm(forms.ModelForm):
     name = forms.CharField(label='name',
                     widget=forms.TextInput(attrs={'placeholder': 'Castle Name'}))
     postcode = forms.CharField(label='postcode', widget=forms.TextInput)
@@ -11,9 +11,9 @@ class CastleCreationFrom(forms.ModelForm):
     house_number = forms.IntegerField(label='house_number',
                     widget=forms.NumberInput(attrs={'placeholder': 'House number'}))
     rooms = forms.CharField(label='rooms', widget=forms.TextInput(attrs={'placeholder': 'commission'}))
-    price = forms.CharField(label='price', widget=forms.TextInput(attrs={'placeholder': 'price'})
-    size = forms.CharField(label='size', widget=forms.TextInput(attrs={'placeholder': 'size'})
-    info = forms.CharField(label='info', widget=forms.TextInput(attrs={'placeholder': 'Describe it in vivid detail'})
+    price = forms.CharField(label='price', widget=forms.TextInput(attrs={'placeholder': 'price'}))
+    size = forms.CharField(label='size', widget=forms.TextInput(attrs={'placeholder': 'size'}))
+    info = forms.CharField(label='info', widget=forms.TextInput(attrs={'placeholder': 'Describe it in vivid detail'}))
 
 
     class Meta:
@@ -31,12 +31,12 @@ class CastleCreationFrom(forms.ModelForm):
 
 
 class CastleImageCreationForm(forms.ModelForm):
-    image = forms.CharField(label='image', widget=forms.TextInput(attrs={'placeholder': 'link to image'})
+    image = forms.CharField(label='image', widget=forms.TextInput(attrs={'placeholder': 'link to image'}))
 
 
     class Meta:
         model = CastleImage
-        fields = ('image')
+        fields = ('image',)
 
     def save(self, user_id, postcode, commit=True):
         profile = super(ProfileCreationForm, self).save(commit=False)
