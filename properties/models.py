068a6a4cb5a9956f42e7d91e0150100ac0b77135
Jóhannes Ingi Torfason <johannest18@ru.is>
2019-05-10 12:53:23 +0000

    tempdata

--- properties/models.py
+++ properties/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
 class Property(models.Model):
@@ -18,4 +19,20 @@ class PropertyImage(models.Model):
     Property = models.ForeignKey(Property, on_delete=models.CASCADE)
     def __str__(self):
         return self.image
-    
+
+class Castle(models.Model):
+    name = models.CharField(max_length=255)
+    zip = models.IntegerField()
+    price = models.IntegerField()
+    commission = models.IntegerField()
+    rooms = models.IntegerField()
+    size = models.IntegerField()
+    verified = models.BooleanField()
+    info = models.TextField()
+    street = models.CharField(max_length=255)
+    house_number = models.IntegerField()
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class CastleImage(models.Model):
+    image = models.CharField(max_length=999)
+    castle = models.ForeignKey(Castle, on_delete=models.CASCADE)
