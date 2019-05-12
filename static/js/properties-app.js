524b40565d4fd8762f331f961438dec6c6a300ab
Jóhannes Ingi Torfason <johannest18@ru.is>
2019-05-12 19:27:16 +0000

    laga lúkkið á search

--- static/js/properties-app.js
+++ static/js/properties-app.js
@@ -26,11 +26,15 @@ $(document).ready(function () {
             success: function (resp) {
                 var newHTML = resp.data.map(d => {
                     return `
-                    <div class="well castles">
+                    <div class="col-md-3 well castles">
                         <a href="/properties/${d.id}">
-                            <img src='${d.firstimage}'>
-                            <h4> ${d.name}</h4>
-                            <p> ${d.info}</p>
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top"  alt="Thumbnail [100%x225]" src='${d.firstimage}'data-holder-rendered="true">
+                            <div class="card-body">
+                                <h1 class="card-text" id="castle-name"> ${d.name}</h1>
+                                <p class="card-text"> ${d.info}</p>
+                            </div>
+                        </div>
                         </a>
                     </div>`
                 });
