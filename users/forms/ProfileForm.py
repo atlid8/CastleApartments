31621c6e0ead8a8c91def21821de9a9543863ea9
Jóhannes Ingi Torfason <johannest18@ru.is>
2019-05-12 16:05:00 +0000

    edit komið

--- users/forms/ProfileForm.py
+++ users/forms/ProfileForm.py
@@ -5,17 +5,19 @@ from django.contrib.auth.models import User
 from users.models import Profile, Postcode
 
 class ProfileForm(ModelForm):
-    first_name = forms.CharField(label='first_name',
-                                 widget=forms.TextInput(
-                                     attrs={'placeholder': 'first name', 'class': 'form-input-field', 'value': 'profile.user.first_name'}))
-    last_name = forms.CharField(label='last_name',
-                                widget=forms.TextInput(attrs={'placeholder': 'last name', 'class': 'form-input-field'}))
-
     class Meta:
             model = Profile
             exclude = ['user', 'id']
             widgets = {
             'profile_image': widgets.TextInput(attrs={'class': 'form-control'}),
+        }
+
+
+class UserEditForm(ModelForm):
+    class Meta:
+            model = User
+            exclude = ['id', 'password', 'last_login', 'is_superuser', 'username', 'email', 'is_staff', 'is_active', 'date_joined', 'groups', 'user_permissions']
+            widgets = {
                 'first_name': widgets.TextInput(attrs={'class': 'form-control'}),
                 'last_name':widgets.TextInput(attrs={'class': 'form-control'}),
         }
