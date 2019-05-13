2e2958839cda1b425d629bce35d1e8df0583526c
Aunnsteinsson <atliunnsteinsson@gmail.com>
2019-05-13 14:39:59 +0000

    yas

--- properties/forms/contactinfo.py
+++ properties/forms/contactinfo.py
@@ -4,22 +4,17 @@ from properties.models import ContactInfo
 
 
 class ContactInfoCreationFrom(forms.ModelForm):
-    name = forms.CharField(label='name',
-                    widget=forms.TextInput(attrs={'placeholder': 'castle name'}))
-    postcode = forms.CharField(label='postcode', widget=forms.TextInput(attrs={'placeholder': 'zip code', 'disabled':'disabled'}))
-    street = forms.CharField(label='street name',
-                    widget=forms.TextInput(attrs={'placeholder': 'street name'}))
-    house_number = forms.IntegerField(label='house_number',
-                    widget=forms.NumberInput(attrs={'placeholder': 'house number'}))
-    rooms = forms.CharField(label='rooms', widget=forms.TextInput(attrs={'placeholder': 'rooms'}))
-    price = forms.CharField(label='price', widget=forms.TextInput(attrs={'placeholder': 'price'}))
-    size = forms.CharField(label='size', widget=forms.TextInput(attrs={'placeholder': 'size (m2)'}))
-    info = forms.CharField(label='info', widget=forms.TextInput(attrs={'placeholder': 'describe it in vivid detail'}))
+    postcode = forms.IntegerField(label='postcode', widget=forms.TextInput(attrs={'placeholder': 'zip code'}))
+    street = forms.CharField(label='street_name', widget=forms.TextInput(attrs={'placeholder': 'street name'}))
+    house_number = forms.IntegerField(label='house_number', widget=forms.NumberInput(attrs={'placeholder': 'house number'}))
+    city = forms.CharField(label='city', widget=forms.TextInput(attrs={'placeholder': 'city'}))
+    ssn = forms.IntegerField(label='ssn', widget=forms.NumberInput(attrs={'placeholder': 'social security number'}))
+    country = forms.ChoiceField(label='country', widget=forms.Select(attrs={'placeholder': 'country'}))
 
 
     class Meta:
-        model = Castle
-        fields = ('name', 'house_number', 'street', 'rooms', 'price', 'size', 'info')
+        model = ContactInfo
+        fields = ('postcode', 'street_name', 'house_number', 'city', 'ssn', 'country')
 
 
     def save(self, seller, verified, postcode, commission, commit=True):
