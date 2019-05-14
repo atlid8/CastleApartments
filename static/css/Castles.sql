d7f4de3327c7af4a49be8bcc7dd7f96aeb5d5808
bbenediktssen <bennibje@gmail.com>
2019-05-14 18:15:46 +0000

    Added more castles

--- static/css/Castles.sql
+++ static/css/Castles.sql
@@ -1,5 +1,29 @@
 insert into properties_castle(id, name, postcode_id, price, commission, rooms, size, verified, info, street, house_number, seller_id) values (3, 'Highgarden', 301, 50000, 5000, 24, 1200, false, 'You can get high in our garden', 'bloomstreet', 11, 53);
 insert into properties_castleimage(id, image, castle_id) values (1, 'https://vignette.wikia.nocookie.net/gameofthrones/images/3/3f/H%26L_Highgarden_.png/revision/latest?cb=20171215221049', 3);
 
-insert into properties_castle(id, name, postcode_id, price, commission, rooms, size, verified, info, street, house_number, seller_id) values (4, 'Storms End', 301, 50000, 5000, 24, 1200, false, 'You can get high in our garden', 'bloomstreet', 11, 53);
-insert into properties_castleimage(id, image, castle_id) values (1, 'https://vignette.wikia.nocookie.net/gameofthrones/images/6/63/HL_Storm%27s_End.jpg/revision/latest?cb=20171213214025', 4);
+insert into properties_castle(id, name, postcode_id, price, commission, rooms, size, verified, info, street, house_number, seller_id) values (4, 'Harrenhall', 301, 27500, 2750, 16, 800, false, 'Harrenhal is a huge castle, the largest one in all of Westeros, though it is also the most ill-omened. It is located on the northern shore of the Gods Eye lake at the heart of the Riverlands, south of the River Trident and northwest of King''s Landing.', 'harrenstreet', 21, 39);
+insert into properties_castleimage(id, image, castle_id) values (2, 'https://vignette.wikia.nocookie.net/gameofthrones/images/9/9b/Harrenhal.jpg/revision/latest?cb=20150328214605', 4);
+
+insert into properties_castle(id, name, postcode_id, price, commission, rooms, size, verified, info, street, house_number, seller_id) values (5, 'Seagard', 301, 31000, 3100, 31, 1400, true, 'Seagard is a castle in the Riverlands. It is the seat of House Mallister, a vassal house holding fealty to House Tully of Riverrun.', 'seastreet', 7, 40);
+insert into properties_castleimage(id, image, castle_id) values (10, 'https://vignette.wikia.nocookie.net/gameofthrones/images/a/a8/Seagard_attack.png/revision/latest?cb=20130416031928', 5);
+
+insert into properties_castle(id, name, postcode_id, price, commission, rooms, size, verified, info, street, house_number, seller_id) values (6, 'Greywater Watch', 501, 16000, 1600, 8, 600, true, 'Greywater Watch is a castle in the Neck. It is ruled by House Reed, a vassal house holding fealty to House Stark of Winterfell.', 'waterroad', 2, 41);
+insert into properties_castleimage(id, image, castle_id) values (4, 'https://vignette.wikia.nocookie.net/gameofthrones/images/3/3f/Greywater_Watch_moves.png/revision/latest?cb=20140220024806', 6);
+
+insert into properties_castle(id, name, postcode_id, price, commission, rooms, size, verified, info, street, house_number, seller_id) values (7, 'Highpoint', 501, 22000, 2200, 13, 1100, true, 'Highpoint is a stone castle situated in the North of Westeros. It is the seat of House Whitehill, staunch vassals of House Bolton.', 'highpoint', 1, 42);
+insert into properties_castleimage(id, image, castle_id) values (5, 'https://vignette.wikia.nocookie.net/gameofthrones/images/5/50/SOW_Highpoint_Art.png/revision/latest?cb=20150521180429', 7);
+
+insert into properties_castle(id, name, postcode_id, price, commission, rooms, size, verified, info, street, house_number, seller_id) values (8, 'Ironrath', 501, 11000, 1100, 5, 500, true, 'Ironrath is a castle situated in the Wolfswood in the North of Westeros. It is the seat of House Forrester, loyal vassals of House Glover of Deepwood Motte.', 'ironroad', 21, 43);
+insert into properties_castleimage(id, image, castle_id) values (6, 'https://vignette.wikia.nocookie.net/gameofthrones/images/1/1d/NOV_Ironrath.png/revision/latest?cb=20151122124005', 8);
+
+insert into properties_castle(id, name, postcode_id, price, commission, rooms, size, verified, info, street, house_number, seller_id) values (9, 'Blackhaven', 201, 25999, 2599, 21, 1400, false, 'Blackhaven is a castle in the Dornish Marches, in the southwestern Stormlands. It is the seat of House Dondarrion, a vassal house holding fealty to House Baratheon of Storms End', 'havenstreet', 13, 44);
+insert into properties_castleimage(id, image, castle_id) values (7, 'https://vignette.wikia.nocookie.net/gameofthrones/images/d/dc/Blackhaven.jpg/revision/latest?cb=20140218214529', 9);
+
+insert into properties_castle(id, name, postcode_id, price, commission, rooms, size, verified, info, street, house_number, seller_id) values (10, 'Starfall', 401, 61000, 6100, 36, 1400, false, 'Starfall is a castle in Dorne, the region controlled by House Martell of Sunspear, and the seat of House Dayne', 'starstreet', 1, 45);
+insert into properties_castleimage(id, image, castle_id) values (8, 'https://vignette.wikia.nocookie.net/gameofthrones/images/1/17/Distant_view_of_Starfall.png/revision/latest?cb=20161121215102', 10);
+
+insert into properties_castle(id, name, postcode_id, price, commission, rooms, size, verified, info, street, house_number, seller_id) values (11, 'Water Gardens', 401, 87000, 8700, 50, 2000, true, 'The Water Gardens is a palace in Dorne that served as a private residence to House Martell of Sunspear, the rulers of Dorne. It is located on the coast a short distance down the road from Sunspear itself.', 'Watterstreet', 4, 46);
+insert into properties_castleimage(id, image, castle_id) values (9, 'https://vignette.wikia.nocookie.net/gameofthrones/images/7/71/Dorne_Season_5.png/revision/latest?cb=20150130222749', 11);
+
+insert into properties_castle(id, name, postcode_id, price, commission, rooms, size, verified, info, street, house_number, seller_id) values (12, 'Cleganes Keep', 210, 44000, 4400, 41, 1550, false, 'Clegane''s Keep is a castle in the Westerlands. It is the seat of House Clegane, a knightly house holding fealty to House Lannister of Casterly Rock.', 'Keeproad', 6, 47);
+insert into properties_castleimage(id, image, castle_id) values (11, 'https://vignette.wikia.nocookie.net/gameofthrones/images/8/8c/Clegane%27s_Keep.png/revision/latest?cb=20130603104012', 12);
