de7dbae4dda620c5be51219855794813cecd8013
bbenediktssen <bennibje@gmail.com>
2019-05-14 17:29:00 +0000

    added file castles.sql

---
+++ static/css/Castles.sql
@@ -0,0 +1,5 @@
+insert into properties_castle(id, name, postcode_id, price, commission, rooms, size, verified, info, street, house_number, seller_id) values (3, 'Highgarden', 301, 50000, 5000, 24, 1200, false, 'You can get high in our garden', 'bloomstreet', 11, 53);
+insert into properties_castleimage(id, image, castle_id) values (1, 'https://vignette.wikia.nocookie.net/gameofthrones/images/3/3f/H%26L_Highgarden_.png/revision/latest?cb=20171215221049', 3);
+
+insert into properties_castle(id, name, postcode_id, price, commission, rooms, size, verified, info, street, house_number, seller_id) values (4, 'Storms End', 301, 50000, 5000, 24, 1200, false, 'You can get high in our garden', 'bloomstreet', 11, 53);
+insert into properties_castleimage(id, image, castle_id) values (1, 'https://vignette.wikia.nocookie.net/gameofthrones/images/6/63/HL_Storm%27s_End.jpg/revision/latest?cb=20171213214025', 4);
