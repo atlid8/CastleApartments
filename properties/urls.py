ec55a5536ea48e9c4f93ea9c95b26f26db060b6b
Jóhannes Ingi Torfason <johannest18@ru.is>
2019-05-16 16:48:32 +0000

    laga urls

--- properties/urls.py
+++ properties/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name="base-index"),
+    path('', views.properties, name="properties-index"),
     path('search/', views.properties, name="properties-index"),
     path('<int:id>/', views.get_property_by_id, name="property_details"),
     path('<int:id>/payment-info/', views.payments, name="payments"),
