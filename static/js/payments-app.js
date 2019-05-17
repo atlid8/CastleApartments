de9c524525ce10b46978d632e85fbdf3c0eb689a
Theodorkri <theodorkri15@ru.is>
2019-05-17 13:18:54 +0000

    Smá vinna með receipt

--- static/js/payments-app.js
+++ static/js/payments-app.js
@@ -1,20 +1,3 @@
-// function get_date() {
-//     let date = new Date();
-//     let year = date.getFullYear();
-//     let month = date.getMonth()+1;
-//     let day = date.getDate();
-//
-//     if (month < 10) {
-//         month = "0" + month
-//     }
-//
-//      if (day < 10) {
-//         day = "0" + day
-//     }
-//
-//     return day + "/" + month + "/" + year
-// }
-
 function get_tax() {
     let price = document.getElementById("subtotal").innerHTML;
     return Math.round(Number(price) * 0.029).toString()
