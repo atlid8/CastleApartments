1012ae8ebe14623af9de53868e64eca70ceaa4d9
bbenediktssen <bennibje@gmail.com>
2019-05-17 14:45:03 +0000

    a

--- static/js/admin-app.js
+++ static/js/admin-app.js
@@ -1,5 +1,5 @@
 function openColumn(evt, column) {
-  var i, tabcontent, tablinks;
+  let i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
